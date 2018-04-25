d96bc8397cf9e81874039adc23e82e636ad9a677
Maciej Sobczynski <gryfon1997@gmail.com>
2018-04-25 22:01:08 +0200

    Implementing corrections #1

--- examples.m
+++ examples.m
@@ -18,3 +18,18 @@ b=transpose([1 3 4 5 2 0]);
 A
 b
 x=LLT(A,b)
+
+disp('----------------------Example 4----------------------');
+n=10;
+b=zeros(1,n-1);
+a=zeros(1,n);
+for i=1:n-1
+    b(1,i)=randn;
+end
+a(1,1)=abs(b(1,1))+abs(rand);
+a(1,n)=abs(b(1,n-1))+abs(rand);
+for i=2:n-1
+    a(1,i)=abs(b(1,i-1))+abs(b(1,i))+abs(rand);
+end
+L=diag(b,-1)+diag(b,1)+diag(a);
+x=LLT(L,transpose(1:n))
